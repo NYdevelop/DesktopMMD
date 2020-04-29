39da87d59b2935db1452064e97f14607dac1948a
gtsrai <gt.srai@gmail.com>
2020-04-29 17:11:11 +0900

    コミット誤り修正

--- main.cpp
+++ main.cpp
@@ -11,9 +11,6 @@ using namespace std;
 // プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
 int main()
 {
-    system("python ../../python/yahooAPI/yahooAPI.py");
-    system("move text.txt data");
-    system("move sep_text.txt data");
     {
         ManageMMD manager;
         manager.Initialize();
